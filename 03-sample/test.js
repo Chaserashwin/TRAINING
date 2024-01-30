f1f6c5da15037b97e3c77295d8d57bb434becb67
Ashwin Jaiswal <jaiswalashwin398@gmail.com>
2024-01-30 11:19:25 +0530

    30-01-2024

---
+++ 03-sample/test.js
@@ -0,0 +1,35 @@
+// callback function
+test.addEventListener('click', function() {
+
+})
+
+// used when we need to call the function when we want to use not with the flow of the program
+
+
+// JSON
+https://test-api.json
+
+fetch(url, {method}, headers)  
+    .then()
+    .catch(error){
+
+    }
+
+// arrow function vs normal function
+const navbar = () => {
+    return (
+        <h1>Test</h1>
+    )
+}
+
+
+function navbar() {
+    return <h1>Test</h1>
+}
+
+//components
+const navbar = () => {
+    return (
+        <h1>Test</h1>
+    )
+}
