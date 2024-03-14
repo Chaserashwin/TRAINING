dec345038780997ec0a3ed59ad01232cd28df5ec
Ashwin Jaiswal <jaiswalashwin398@gmail.com>
2024-03-14 23:59:25 +0530

    new

--- 3-DATABASE/3-Intro_to_Schemas/2-Evaluation_Query_Methods_in_Schemas/text.mongodb.js
+++
@@ -1,7 +0,0 @@
-use("teacher");
-
-db.monthlyBudget.createIndex({ category: 1 });
-
-db.monthlyBudget.createIndex({ category: "text" });
-
-db.monthlyBudget.createIndex({ $text: { $search: "food" } });
