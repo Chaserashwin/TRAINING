9df74d410f542bbffb1483dd6bd1bb712013f8c0
Ashwin Jaiswal <jaiswalashwin398@gmail.com>
2024-03-14 19:48:09 +0530

    new

--- 1-FRONTEND/Extra/TAILWIND/tailwind.config.js
+++ 1-FRONTEND/Extra/TAILWIND/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}.Basics"],
+  content: ["./Basics/*.html", "*.html"],
   theme: {
     extend: {},
   },
