c59a524310a10fe2c1614bf7c6d8a824fddbb460
Ashwin Jaiswal <jaiswalashwin398@gmail.com>
2024-03-27 16:45:05 +0530

    new

--- 3-DATABASE/09-test/frontend/src/main.jsx
+++ 3-DATABASE/09-test/frontend/src/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM, { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-// import './index.css'
+import "./style/index.css";
+import "./style/tailwind.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(<App />);
